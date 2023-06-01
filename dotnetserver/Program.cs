bc50a3f6fb995f37257fdbd57c3015541b271076
bjorn-einar-bjartnes-4ss <bjorn.einar.bjartnes@4subsea.com>
2023-06-01 11:08:55 +0200

    Some sample code to cancel requests in .net when connection is closed

--- dotnetserver/Program.cs
+++ dotnetserver/Program.cs
@@ -2,4 +2,31 @@ var app = WebApplication.Create(args);
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/cancelNotPassingCt", async (CancellationToken ct, ILogger<Program> logger) => 
+ 	{
+		try {
+			logger.LogInformation("Incoming request");
+			await Task.Delay(10000);
+			logger.LogInformation("Finished waiting");
+			return "Hello World!";
+		} catch (OperationCanceledException ex) {
+			logger.LogError("I was cancelled", ex);
+			return "I was cancelled...";
+		}
+ 	});
+
+app.MapGet("/cancelPassingCt", async (CancellationToken ct, ILogger<Program> logger) => 
+ 	{
+		try {
+			logger.LogInformation("Incoming request");
+			await Task.Delay(10000, ct);
+			logger.LogInformation("Finished waiting");
+			return "Hello World!";
+		} catch (OperationCanceledException ex) {
+			logger.LogError("I was cancelled", ex);
+			return "I was cancelled...";
+		}
+ 	});
+
+
 app.Run("http://localhost:3000");
