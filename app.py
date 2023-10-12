24c1b8b3146a3fb193d886d28d72fd7d7c368b62
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-10-12 14:03:03 +0000

    Fail after a while

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/failafterawhile")
+def generate():
+    for i in range(10):
+        yield f"{i}, {i*i}\n"
+        if i > 8:
+            raise OverflowError()
+    return generate()
