c9ffe91d258c2d9268d501dcfb010a50a17a8822
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-10-13 13:49:36 +0200

    Create integration test

---
+++ flask_integration_test.py
@@ -0,0 +1,22 @@
+import threading
+import time
+import requests
+import pytest
+from app import app
+
+@pytest.fixture(scope='module', autouse=True)
+def start_flask_app():
+    server_thread = threading.Thread(target=app.run, kwargs={'host': 'localhost', 'port': 5010})
+    server_thread.start()
+    time.sleep(1)
+    yield
+    server_thread.join()
+
+def test_short_delays():
+    response = requests.get('http://localhost:5010/hang/0.01')
+    assert response.status_code == 200
+    assert "9, 81" in response.text
+
+def test_longer_delays():
+    with pytest.raises(requests.exceptions.ConnectionError):
+        response = requests.get('http://localhost:5010/hang/0.3', timeout=0.2)
