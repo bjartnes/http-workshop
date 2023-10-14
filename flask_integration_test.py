9534fa3f181cba6e0199c6467788cc0a0839aeba
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-10-14 20:37:14 +0200

    retry with the retries

--- flask_integration_test.py
+++ flask_integration_test.py
@@ -2,9 +2,25 @@ import subprocess
 import time
 import requests
 import pytest
+from requests.adapters import HTTPAdapter
 from retry import retry
+from urllib3 import Retry
 from app import app
 import os
+
+def retry_session(retries, session=None, backoff_factor=3, backoff_max=3):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        backoff_max = backoff_max
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 @pytest.fixture(scope='module', autouse=True)
 def start_flask_app():
@@ -17,7 +33,7 @@ def start_flask_app():
     flask_process.kill()
 
 def test_short_delays():
-    response = requests.get('http://localhost:5010/hang/0.01')
+    response = retry_session(3).get('http://localhost:5010/hang/0.01')
     assert response.status_code == 200
     assert "9, 81" in response.text
 
@@ -28,11 +44,11 @@ def retry_my_function():
     return get_with_delay(pytest.__retry_delay) 
 
 def get_with_delay(delay):
-    return requests.get('http://localhost:5010/hang/' + str(delay), timeout=0.11)
+    return retry_session(3).get('http://localhost:5010/hang/' + str(delay), timeout=0.11)
 
 def test_longer_delays():
     with pytest.raises(requests.exceptions.ConnectionError):
-        response = requests.get('http://localhost:5010/hang/0.2', timeout=0.1)
+        response = retry_session(3).get('http://localhost:5010/hang/0.2', timeout=0.1)
 
 def test_longer_delays_with_retry():
     response = retry_my_function() 
